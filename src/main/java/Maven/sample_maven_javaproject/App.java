1366ce7a7f951d7ae32ed0f30697c00c027199c2
Rohith6936 <kannarohith26255@gmail.com>
2025-09-23 10:39:21 +0530

    initial commit

---
+++ src/main/java/Maven/sample_maven_javaproject/App.java
@@ -0,0 +1,10 @@
+package Maven.sample_maven_javaproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
